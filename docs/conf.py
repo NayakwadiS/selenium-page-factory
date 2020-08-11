3f0009cebffe3de18f48ad0a821a9da932503216
NayakwadiS <39816455+NayakwadiS@users.noreply.github.com>
2020-08-11 10:03:51 +0530

    created configuration

---
+++ docs/conf.py
@@ -0,0 +1,22 @@
+from recommonmark.transform import AutoStructify
+from recommonmark.parser import CommonMarkParser
+
+# -- Project information -------------------------
+
+project = 'selenium-page-factory'
+
+# The short X.Y version
+version = ''
+# The full version, including alpha/beta/rc tags
+release = '2.0'
+
+# The master toctree document.
+master_doc = 'index'
+
+html_favicon = 'selenium-page-factory_logo.png'
+
+html_static_path = ['_static']
+
+def setup(app):
+    app.add_stylesheet('css/custom.css?v20200811')
+    
