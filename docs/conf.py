50349ce63b6446bc8982f49efa9b9d75af50498e
Sujit Nayakwadi <39816455+NayakwadiS@users.noreply.github.com>
2020-12-28 14:02:10 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@ project = 'selenium-page-factory'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '2.0'
+release = '2.1'
 
 # The master toctree document.
 master_doc = 'index'
@@ -18,5 +18,5 @@ html_favicon = 'selenium-page-factory_logo.png'
 html_static_path = ['_static']
 
 def setup(app):
-    app.add_stylesheet('css/custom.css?v20200811')
+    app.add_stylesheet('css/custom.css?v20201228')
     
