268ac5d75746473378956dfc82a59b48be42a488
Void_User <39816455+NayakwadiS@users.noreply.github.com>
2023-03-19 11:06:46 +0530

    v2.6

--- docs/conf.py
+++ docs/conf.py
@@ -6,9 +6,9 @@ from recommonmark.parser import CommonMarkParser
 project = 'selenium-page-factory'
 
 # The short X.Y version
-version = '2.5'
+version = '2.6'
 # The full version, including alpha/beta/rc tags
-release = '2.5'
+release = '2.6'
 
 # The master toctree document.
 master_doc = 'index'
@@ -18,5 +18,5 @@ html_favicon = 'selenium-page-factory_logo.png'
 html_static_path = ['_static']
 
 def setup(app):
-    app.add_stylesheet('css/custom.css?v20220811')
+    app.add_stylesheet('css/custom.css?v20230319')
     
